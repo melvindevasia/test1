7b469e0beb8d56b6239ac08a6d0f1473a8560012
melvindevasia <melvindevasiaplpy@gmail.com>
2025-08-04 20:30:51 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function enlargeImage() {
+  var profile = document.getElementById('profilepic');
+  profile.style.transform = 'scale(1.1)';
+}
+
+function fun1(item) {
+  item.style.fontWeight = "bold";
+  item.style.color = "blue";
+}
+
+function fun2(item) {
+  item.style.fontWeight = "normal";
+  item.style.color = "black";
+}
